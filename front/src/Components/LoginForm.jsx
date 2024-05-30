1095bade46145bb7de162d7e9e144a3b2db67dd7
Caeneus60 <alanpy60@gmail.com>
2024-05-30 15:38:05 -0700

    Add connection with backend for login.

--- front/src/Components/LoginForm.jsx
+++ front/src/Components/LoginForm.jsx
@@ -1,23 +1,106 @@
-import { useRef } from "react"
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const LoginForm = () => {
-  const inputRef = useRef()
-  
-  const login = (event) => {
-    alert(inputRef.current.value)
-    event.preventDefault()
+  const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const loginURI = "http://localhost:4000/sesion/login";
+
+  async function checkLogin(url) {
+    const userData = {
+      usuario: username,
+      contrasena: password,
+    };
+
+    const options = {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(userData),
+    };
+
+    try {
+      const response = await fetch(url, options);
+      if (!response.ok) {
+        throw new Error(`Network response was not ok: ${response.statusText}`);
+      }
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.log("Fetch error: ", error);
+      throw error;
+    }
   }
 
-  return (
-    <form onSubmit={login}>
-      <p>Usuario</p>
-      <input type="text" id="user" ref={inputRef} name="usuario" />
-      <p>Contrasena</p>
-      <input type="password" id="pw" ref={inputRef} name="contrasena" />
-      <br></br>
-      <button type="submit">Submit</button>
-    </form>
-  )
-}
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (!username || !password) {
+      alert("Debe ingresar usuario y contraseña");
+      return;
+    }
 
-export default LoginForm
+    try {
+      checkLogin(loginURI);
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  const handleSignUp = () => {
+    navigate("/Registro");
+  };
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  return (
+    <form className="Formulario-Inicio-Sesion" onSubmit={handleSubmit}>
+      <label htmlFor="username" className="user">
+        Username:
+      </label>
+      <input
+        type="text"
+        id="username"
+        name="username"
+        onChange={handleUsernameChange}
+        className="Entrada-Username"
+      />
+      <label htmlFor="password" className="pass">
+        Password:
+      </label>
+      <input
+        type="password"
+        id="password"
+        name="password"
+        onChange={handlePasswordChange}
+        className="Entrada-Password"
+      />
+
+      <div className="pt-4">
+        <p>
+          Aun no tienes cuenta?{" "}
+          <a onClick={handleSignUp} className="font-bold registrate-anchor">
+            Registrate aqui!
+          </a>
+        </p>
+      </div>
+
+      <button
+        type="submit"
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Login
+      </button>
+    </form>
+  );
+};
+
+export default LoginForm;
--- front/src/Pages/Home.jsx
+++ front/src/Pages/Home.jsx
@@ -1,13 +1,9 @@
-import { useNavigate } from "react-router-dom";
 import "../Styles/Home.css";
+import LoginForm from "../Components/LoginForm";
+
+// TODO: Add jwt to local storage and check for authentication.
 
 const Home = () => {
-  const navigate = useNavigate();
-
-  const handleSignUp = () => {
-    navigate("/Registro");
-  };
-
   return (
     <div className="container mx-auto">
       <div className="grid grid-cols-1 px-auto">
@@ -19,45 +15,7 @@ const Home = () => {
         />
         <div className="text-center">
           <h1 className="Titulo-Inicia-Sesion">Inicia sesion</h1>
-          <form className="Formulario-Inicio-Sesion">
-            <label htmlFor="username" className="user">
-              Username:
-            </label>
-            <input
-              type="text"
-              id="username"
-              name="username"
-              className="Entrada-Username"
-            />
-            <label htmlFor="password" className="pass">
-              Password:
-            </label>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              className="Entrada-Password"
-            />
-
-            <div className="pt-4">
-              <p>
-                Aun no tienes cuenta?{" "}
-                <a
-                  onClick={handleSignUp}
-                  className="font-bold registrate-anchor"
-                >
-                  Registrate aqui!
-                </a>
-              </p>
-            </div>
-
-            <button
-              type="submit"
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-            >
-              Login
-            </button>
-          </form>
+          <LoginForm />
         </div>
       </div>
     </div>
