56c5e75d2daf3f3d75725ffe581093adce57f4b3
Angel Olan <angel.edoc23@gmail.com>
2024-06-01 08:47:17 -0700

    Si

---
+++ front/src/Components/ViajesCompletadosCards.jsx
@@ -0,0 +1,39 @@
+const ViajesCards = ({ marca, horallegada, escalas, tiempoestimado, onClick }) => {
+  const handleClick = () => {
+    onClick({ origen, destino, hora });
+  };
+  return (
+    <div
+      className="flex justify-center items-center px-4 py-8"
+      onClick={handleClick}
+    >
+      <div className="max-w-md w-full bg-white rounded-lg shadow-lg overflow-hidden">
+        <div className="bg-blue-500 p-4">
+          <h2 className="text-xl text-white font-semibold text-center">
+            Detalles del Viaje
+          </h2>
+        </div>
+        <div className="p-4">
+          <div className="flex items-center justify-between py-2">
+            <h3 className="text-gray-900 font-semibold">Marca autobus:</h3>
+            <p className="text-gray-600">{marca}</p>
+          </div>
+          <div className="flex items-center justify-between py-2">
+            <h3 className="text-gray-900 font-semibold">Hora de llegada:</h3>
+            <p className="text-gray-600">{horallegada}</p>
+          </div>
+          <div className="flex items-center justify-between py-2">
+            <h3 className="text-gray-900 font-semibold">Escalas:</h3>
+            <p className="text-gray-600">{escalas}</p>
+          </div>
+          <div className="flex items-center justify-between py-2">
+            <h3 className="text-gray-900 font-semibold">Horas de viaje:</h3>
+            <p className="text-gray-600">{tiempoestimado}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ViajesCards;
